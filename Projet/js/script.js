8e54b6f73c4c66c5cdf6203bb7992885f86cd7f7
Gil <gildavidi75@gmail.com>
2022-04-23 23:09:41 +0300

    change object page

--- Projet/js/script.js
+++ Projet/js/script.js
@@ -1,1 +1,29 @@
+$("document").ready(() => {
+    $('.edit_button').click(function() 
+    {
+        let index=  $('.edit_button').index(this);
+        if(index ==0)
+        {
+            $('input[name="begin"]').prop('disabled', false);
+            $('input[name="end"]').prop('disabled', false);
+         }
+         if(index ==1)
+         {
+            $('select[name="Ages"]').prop('disabled', false);
 
+         }
+         if(index ==2)
+         {
+            $('select[name="Character"]').prop('disabled', false);
+        }
+        if(index >2)
+        {
+            index=index-2;
+            if(index ==1)
+            {
+                
+            }  
+
+        }
+    })
+});
