ed13c600d548b38d14eb5cc7cd1953ebd4b51b80
Gil <gildavidi75@gmail.com>
2022-04-26 23:29:05 +0300

    CHAGES AT ALL

--- Projet/js/list_page.js
+++ Projet/js/list_page.js
@@ -1,23 +1,21 @@
 
-(function () {
-    if (window.matchMedia("(max-width: 760px)").matches) {
-        document.getElementsByClassName("hamburger")[0].style.visibility = 'visible';
-        console.log("blabla");
-    } else {
-        console.log("bobo");
-        document.getElementsByClassName("hamburger")[0].style.visibility = 'hidden';
 
+$("document").ready(() => {
+    $('.delete_button').click(function() 
+    {
+        let index=  $('.delete_button').index(this);
+        $('.destnation')[index].css("visibility","hidden");
 
-    }
-})();
-
-
-$(function () {
-
-    // ACTIVATION DU DATEPICKER 
-    $('.datepicker').datepicker({
-        clearBtn: true,
-        format: "dd/mm/yyyy"
-    });
-
+       
+    
+    })
 });
+//$(function () {
+
+     //ACTIVATION DU DATEPICKER 
+  //  $('.datepicker').datepicker({
+    //    clearBtn: true,
+      // format: "dd/mm/yyyy"
+     //});
+//});
+
