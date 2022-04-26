5249a44f03083d9640d34b0420457dbb49d75a78
Gil <gildavidi75@gmail.com>
2022-04-27 00:04:17 +0300

    PUSH FOR MLISA

--- Projet/js/list_page.js
+++ Projet/js/list_page.js
@@ -4,7 +4,7 @@ $("document").ready(() => {
     $('.delete_button').click(function() 
     {
         let index=  $('.delete_button').index(this);
-        $('.destnation')[index].css("visibility","hidden");
+        $('.destnation').eq(index).css("display", "none");
 
        
     
