fb2100113aaf2530e7896e19f7eff5eefeef4a3b
melgabay <77579733+melgabay@users.noreply.github.com>
2022-04-27 17:47:01 +0300

    change on form + push php
    change on form + push php

---
+++ Projet/get_params_login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Added Player</title>
+    </head>
+    <body>
+        <section>
+            <?php
+                echo "<h2>The player has been successfully added</h2>";
+            ?>
+        </section>
+    </body>
+</html>
