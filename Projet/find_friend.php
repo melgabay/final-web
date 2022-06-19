07b88bfba908afd1e690f21cac5199cb036d703c
melgabay <77579733+melgabay@users.noreply.github.com>
2022-06-19 21:10:16 +0300

    change all page of from maybe we take it + js of the form + idk what i change on friend.php lol

--- Projet/find_friend.php
+++ Projet/find_friend.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <link rel="icon" href="favicon.ico">
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -117,7 +117,7 @@
     </header>
 
     <main class="ffmain">
-        <form action="#" method="GET" class="find-friend-page">
+        <form action="#" method="GET" class="rec-white find-friend-page ">
             <h3> How would you like to search for Friend</h3>
 
             <div class="d-flex">
