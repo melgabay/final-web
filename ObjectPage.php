0c3e5a12095c57634d598f307ce8412eea07fa3a
melgabay <77579733+melgabay@users.noreply.github.com>
2022-07-08 14:34:50 +0300

    change form

---
+++ ObjectPage.php
@@ -0,0 +1,233 @@
+<?php 
+
+    //create a mySQL DB connection:
+
+	include "config.php";
+
+
+
+    $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+    
+
+    //testing connection success
+
+    if(mysqli_connect_errno()) {
+
+        die("DB connection failed: " . mysqli_connect_error() . " (" . mysqli_connect_errno() . ")"
+
+        );
+
+    }
+
+?>
+
+<?php 
+
+	//get data from DB
+
+	$prodId = $_GET["prodId"];
+
+	$query 	= "SELECT * from tbl_trips_206 as t
+    inner join tbl_destinations_trip_206 as t_d
+    on t.t_id= t_d.t_id
+    inner join tbl_destination_206 as d
+    on d.d_id= t_d.d_id where t.t_id=" . $prodId;
+	// echo $query;
+
+	$result = mysqli_query($connection, $query);
+
+	if($result) {
+
+		$row 	= mysqli_fetch_assoc($result);
+
+	}
+
+	else die("DB query failed.");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<link rel="icon" href="favicon.ico">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Assistant">
+    <!-- BOOTSRAP -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/object_page.css">
+    <link rel="stylesheet" href="css/template.css">
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <script src="js/script.js"></script>
+    <title>TripDream</title>
+</head>
+
+
+<body>
+
+
+<header class="d-flex justify-content-between">
+     <input type="checkbox" id="hamburger-input" class="burger-shower" />
+        <label id="hamburger-menu" for="hamburger-input">
+        <nav id="sidebar-menu">
+            <h3>Menu</h3>
+            <ul>
+            <li class="nav-item"><a class="nav-link " aria-current="page" href="homepage.php">Homepage</a></li>
+            <li class="nav-item"><a class="nav-link  " href="form.php">Create New Trip</a></li>
+            <li class="nav-item"><a class="nav-link active" href="list_page.php">My Trips</a></li>
+            <li class="nav-item"><a class="nav-link  " href="find_friend.php">Find Friend</a></li>
+            </ul>
+        </nav>
+        </label>
+
+        <div class="overlay"></div>
+        <a href="index.html"><img src="./images/logo.png" alt="logo"></a>
+
+         <nav id="main-menu">
+            <ul class="d-flex justify-content-around align-items-center">
+                <li class="nav-item"><a class="nav-link " aria-current="page" href="homepage.php">Homepage</a></li>
+                <li class="nav-item"><a class="nav-link  " href="form.php">Create New Trip</a></li>
+                <li class="nav-item"><a class="nav-link active" href="list_page.php">My Trips</a></li>
+                <li class="nav-item"><a class="nav-link  " href="find_friend.php">Find Friend</a></li>
+            </ul>
+        </nav>
+
+        
+
+        <section class="prof-pic">
+            <a href="#" class="user_pic"></a>
+            <span>Hello Dana</span>
+        </section>
+    </header>
+
+    <main class="main-form">
+    <div class="trip_details">
+            <section class="trip_details_section">
+                <span>Dates:</span>
+                <input type="date" name="begin" placeholder="dd-mm-yyyy"  min="1997-01-01"
+                    max="2030-12-31" value="<?php echo $row["start_date"];?>" disabled >
+                <input type="date" name="end" placeholder="dd-mm-yyyy" value="<?php echo $row["end_date"];?>" min="1997-01-01"
+                    max="2030-12-31" disabled>
+            </section>
+            <section class="trip_details_section">
+                <span>Ages:</span>
+                <!-- <button class="edit_button"></button>  -->
+                <select name="Ages" disabled data-selected="<?php echo $row["ages"];?>">
+                    <option value="1">18-25</option>
+                    <option value="2">26-35</option>
+                    <option value="3">36-45</option>
+                    <option value="4">46-60</option>
+                    <option value="5">60+</option>
+                </select>
+            </section>
+            <section class="trip_details_section">
+                <span>Character</span>
+                <!-- <button class="edit_button"></button>  -->
+                <select name="Character" disabled  data-selected="<?php echo $row["nature"];?>">
+                    <option value="1">Chill</option>
+                    <option value="2">Sunbathing</option>
+                    <option value="3">Tracks</option>
+                    <option value="4">Nature</option>
+                    <option value="5">Field</option>
+                    <option value="6">Shopping</option>
+                </select>
+            </section>
+            <section class="trip_details_section">
+                <span>Total</span>
+                <span><?php echo $row["budget"];?> &nbsp;&#8362;</span>
+            </section>
+    </div>
+        <h2 id="object_page_h2">Path:</h2>
+        <div id="trip_destnations" class="trip_destnations1">
+            <?php 
+        $numberDest=0;
+        while($row) 
+                {
+                $numberDest++;
+                echo '<div class="destnation">';
+                echo         '<div class="des_img">';
+                echo             '<img src="images/Vector 3.png">';
+                echo             '<img src="images/dest'.$numberDest.'.png">';
+                echo              '<img src="images/dots.png">';
+                echo             '<img src="images/bus.png">';
+                echo          '</div>';
+                echo          '<div class="area_list">';
+                echo            '<h3>'. $row["destination_name"] .'</h3>';
+                echo             '<img src="images/Vector 4.png">';
+                echo             '<span>Areas:</span>';
+                echo             '<ol>';
+                $queryArea 	= "SELECT a.area_name,a.a_id from tbl_destination_206 as d
+                inner join tbl_destination_area_206  as d_a
+                on d.d_id=d_a.d_id
+                inner join tbl_area_206 as a
+                on d_a.a_id= a.a_id where d.d_id=" . $row["d_id"]."";
+                $resultArea = mysqli_query($connection, $queryArea);
+                if($resultArea) {
+
+                    $rowArea= mysqli_fetch_assoc($resultArea);
+            
+                }
+                else die("DB query failed.");
+                while($rowArea) 
+                {
+                echo                 '<li>';
+                echo                     '<div class="form-check">';
+                echo    '<input class="form-check-input" type="checkbox" name="dest1_ares[]" value="' . $row["d_id"].'" id="'.  $rowArea["a_id"].'" checked>';
+                echo    '<label class="form-check-label"> '.  $rowArea["area_name"].'</label>';
+                echo                     '</div>';
+                echo                 '</li>';
+                $rowArea= mysqli_fetch_assoc($resultArea);
+                 }
+                echo             '</ol>';
+                echo             '<section>';
+                echo                '<p>Total:7000 &nbsp;&#8362;</p>';
+                echo            '</section>';
+                echo         '</div>';
+                echo     '</div>';
+                $row= mysqli_fetch_assoc($result);
+                }
+        ?> 
+        
+        </div>
+
+        <div class="change-button">
+            <button type="button" class="btn btn-secondary" id="delete_button" >Delete</button>
+            <div class="edit" id="edit_button"> <button type="button" class="btn btn-secondary">Edit</button></div>
+        </div>
+        <div id="myModal" class="modal fade" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Message</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+            <div class="modal-body">
+                <p id="mes_text">Are you sure you want to delete this trip?</p>
+                <p class="text-secondary"><small></small></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="edit_button_mes">Cancel</button>
+                <button type="button" class="btn btn-primary" id="delete_button_mes">Delete</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+    </main>
+</body>
+
+</html>
+<?php
+
+//close DB connection
+
+mysqli_close($connection);
+
+?>
---
+++ form.php
@@ -0,0 +1,321 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="icon" href="favicon.ico">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Assistant">
+
+    <!-- boostrap css -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- bootsrap js -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+
+
+
+    <!-- BOOTSRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- css -->
+
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">-->
+    <link rel="stylesheet" href="css/template.css">
+    <!-- <link rel="stylesheet" href="css/form.css"> -->
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>-->
+    <script defer="" src="js/list_page.js"></script>
+    <script defer="" src="js/script.js"></script>
+    <script defer="" src="js/addform.js"></script>
+
+
+
+    <!-- calendar -->
+    <!-- jQuery -->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+
+    <!-- Isolated Version of Bootstrap, not needed if your site already uses Bootstrap -->
+    <link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" />
+
+    <!-- Bootstrap Date-Picker Plugin -->
+    <script type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css" />
+
+    <!-- calendar -->
+
+    <title>TripDream</title>
+</head>
+
+
+<body>
+
+  
+<header class="d-flex justify-content-between">
+    <input type="checkbox" id="hamburger-input" class="burger-shower" >
+        <label id="hamburger-menu" for="hamburger-input">
+        <nav id="sidebar-menu">
+            <h3>Menu</h3>
+            <ul>
+            <li class="nav-item"><a class="nav-link " aria-current="page" href="homepage.php">Homepage</a></li>
+            <li class="nav-item"><a class="nav-link active " href="form.php">Create New Trip</a></li>
+            <li class="nav-item"><a class="nav-link " href="list_page.php">My Trips</a></li>
+            <li class="nav-item"><a class="nav-link  " href="find_friend.php">Find Friend</a></li>
+            </ul>
+        </nav>
+        </label>
+
+        <div class="overlay"></div>
+        <a href="index.html"><img src="./images/logo.png" alt="logo"></a>
+
+         <nav id="main-menu">
+            <ul class="d-flex justify-content-around align-items-center">
+                <li class="nav-item"><a class="nav-link " aria-current="page" href="homepage.php">Homepage</a></li>
+                <li class="nav-item"><a class="nav-link active " href="form.php">Create New Trip</a></li>
+                <li class="nav-item"><a class="nav-link " href="list_page.php">My Trips</a></li>
+                <li class="nav-item"><a class="nav-link  " href="find_friend.php">Find Friend</a></li>
+            </ul>
+        </nav>
+
+        
+
+        <section class="prof-pic">
+            <a href="#" class="user_pic"></a>
+            <span>Hello Dana</span>
+        </section>
+</header>
+
+<main class="formpage">
+    <h2> Create New Trip </h2>
+    <div class="d-flex justify-content-center  mt-5">
+        <form action="#" method="GET" class="rec-white col-md-8"> 
+            <div class="d-flex fp justify-content-around">
+                <div class="col-md-3">
+                    <label> Amount of trip</label>
+                    <input type="number" class="form-control" id="amount" placeholder="0 $" min="0">
+                </div>
+
+                <div class="col-md-3">
+                    <label> Type of trip</label>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" name="TypeofTrip" value="Nature">
+                        <label class="form-check-label" for="flexCheckDefault">Nature</label>
+                    </div>
+
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" name="TypeofTrip" value="Beach">
+                        <label class="form-check-label" for="flexCheckDefault">Beach</label>
+                    </div>
+
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" name="TypeofTrip" value="Sport">
+                        <label class="form-check-label" for="flexCheckDefault">Sport</label>
+                    </div>
+
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" name="TypeofTrip" value="Quiet">
+                        <label class="form-check-label" for="flexCheckDefault">Quiet</label>
+                    </div>
+
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" name="TypeofTrip" value="Party">
+                        <label class="form-check-label" for="flexCheckDefault">Party</label>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <label> Select the age</label>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="age" value="18!">
+                        <label class="form-check-label">18-25</label>
+                    </div>
+
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="age" value="25">
+                        <label class="form-check-label">25-35</label>
+                    </div>
+
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="age" value="35">
+                        <label class="form-check-label">35-45</label>
+                    </div>
+
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="age" value="45">
+                        <label class="form-check-label">45-60</label>
+                    </div>
+
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="age" value="60">
+                        <label class="form-check-label">60 +</label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="beige-rec pb-5 pt-3 mt-4" id="form-part-two">
+                <div class="d-flex fp justify-content-around">
+                        <div class="col-md-3">
+                            <label for="inputState" class="form-label">Choose continent</label>
+                            <select id="inputState" class="form-select" required>
+                                <option value="">...</option>
+                                <option value="South America">South America</option>
+                                <option value="North America">North America</option>
+                                <option value="Europe">Europe</option>
+                                <option value="Asia">Asia</option>
+                                <option value="Australia">Australia</option>
+                                <option value="Africa">Africa</option>
+                            </select>
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="inputState" class="form-label">Choose Country</label>
+                            <select id="inputState" class="form-select" required>
+                                <option value="">...</option>
+                                <option value="Bresilia">Bresilia</option>
+                                <option value="Paraguay">Paraguay</option>
+                                <option value="Argentina">Argentina</option>
+                                <option value="Urugay">Urugay</option>
+                                <option value="Perou">Perou</option>
+                                <option value="Mexique">Mexique</option>
+                            </select>
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="inputState" class="form-label">Choose Town</label>
+                            <select id="inputState" class="form-select" required>
+                                <option value="">...</option>
+                                <!-- IF THE COUNTRY CHOOSE IS BRASILIA  -->
+                                <option value="Brasilia">Brasilia</option>
+                                <option value="Rio de Janero">Rio de Janero</option>
+                                <option value="Sao Paulo">Sao Paulo</option>
+                                <option value="Salvador da Bahia">Salvador da Bahia</option>
+                                <option value="Manaus">Manaus</option>
+                            </select>
+                        </div>
+                </div>
+
+                <div class="d-flex fp justify-content-around">
+                    <div class="col-md-3 budget-in">
+                        <label class="control-label" for="budget">Enter a budget</label>
+                        <input type="number" class="form-control" id="inputPassword2" placeholder="0 $" min="0">
+                    </div>
+
+                    <div class="col-md-3">
+                        <label class="control-label" for="date">Start Date</label>
+                        <input class="form-control" id="date" name="date" placeholder="MM/DD/YYY" type="text" />
+                    </div>
+
+
+                    <div class="col-md-3">
+                        <label class="control-label" for="date">End date</label>
+                        <input class="form-control" id="date" name="date" placeholder="MM/DD/YYY" type="text" />
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="d-block mt-5 pb-5" id="pre">
+
+                <button type="button" class="btn btn-outline-success" id="count">
+                    Add destination
+                    <img src="./images/PLUS.png" alt="plus">
+                </button>
+
+                <button type="button" class="btn btn-primary new-color bouton-form">Search</button>
+            </div>
+        </form>
+    </div>
+        <!-- <form action="#" method="GET" class="form-page-form">
+            <h3> Choose destination:</h3>
+            <div class="rec-white form-dest-page " id="form">
+                <div class="d-flex fp justify-content-around">
+                    <div class="col-md-3">
+                        <label for="inputState" class="form-label">Choose continent</label>
+                        <select id="inputState" class="form-select" required>
+                            <option value="">...</option>
+                            <option value="South America">South America</option>
+                            <option value="North America">North America</option>
+                            <option value="Europe">Europe</option>
+                            <option value="Asia">Asia</option>
+                            <option value="Australia">Australia</option>
+                            <option value="Africa">Africa</option>
+                        </select>
+                    </div>
+
+                    <div class="col-md-3">
+                        <label for="inputState" class="form-label">Choose Country</label>
+                        <select id="inputState" class="form-select" required>
+                            <option value="">...</option>
+                            <option value="Bresilia">Bresilia</option>
+                            <option value="Paraguay">Paraguay</option>
+                            <option value="Argentina">Argentina</option>
+                            <option value="Urugay">Urugay</option>
+                            <option value="Perou">Perou</option>
+                            <option value="Mexique">Mexique</option>
+                        </select>
+                    </div>
+
+                    <div class="col-md-3">
+                        <label for="inputState" class="form-label">Choose Town</label>
+                        <select id="inputState" class="form-select" required>
+                            <option value="">...</option>
+                            <!-- IF THE COUNTRY CHOOSE IS BRASILIA 
+                            <option value="Brasilia">Brasilia</option>
+                            <option value="Rio de Janero">Rio de Janero</option>
+                            <option value="Sao Paulo">Sao Paulo</option>
+                            <option value="Salvador da Bahia">Salvador da Bahia</option>
+                            <option value="Manaus">Manaus</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="d-flex fp justify-content-around">
+                    <div class="col-md-3 budget-in">
+                        <label class="control-label" for="budget">Enter a budget</label>
+                        <input type="number" class="form-control" id="inputPassword2" placeholder="0 $" min="0">
+                    </div>
+
+                    <div class="col-md-3">
+                        <label class="control-label" for="date">Start Date</label>
+                        <input class="form-control" id="date" name="date" placeholder="MM/DD/YYY" type="text" />
+                    </div>
+
+
+                    <div class="col-md-3">
+                        <label class="control-label" for="date">End date</label>
+                        <input class="form-control" id="date" name="date" placeholder="MM/DD/YYY" type="text" />
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class=" d-block" id="pre">
+                <button type="button" class="btn btn-outline-success" id="count">
+                    Add destination
+                    <img src="./images/PLUS.png" alt="plus">
+                </button>
+
+                <button type="button" class="btn btn-primary new-color bouton-form">Search</button>
+            </div>
+        </form> -->
+    </main>
+</body>
+
+</html>
