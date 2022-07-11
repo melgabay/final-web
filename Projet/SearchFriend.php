d01c69556e49fedc5a1e4cf85c2a6deed3b16660
melgabay <77579733+melgabay@users.noreply.github.com>
2022-07-11 20:02:58 +0300

    push of all because of bug

--- Projet/SearchFriend.php
+++ Projet/SearchFriend.php
@@ -25,29 +25,41 @@
 <?php 
 
 	//get data from DB
-    $dateYear=0;
-    $dateMonth=0;
-
-
-    if(isset( $_GET["dateYear"]))
+    $begin=0;
+    $end=0;
+    $ages=0;
+    $character =0;
+	$prodId = $_GET["prodId"];
+    if(isset( $_GET["begin"]))
     {
-    $dateYear = $_GET["dateYear"]; 
+    $begin = $_GET["begin"]; 
+    }
+    if(isset( $_GET["end"]))
+    {
+    $end = $_GET["end"];
+    }
+    if(isset( $_GET["ages"]))
+    {
+    $ages = $_GET["ages"];
+    }
+    if(isset($_GET["character"]))
+    {
+    $character = $_GET["character"];
     }
 
-    if(isset( $_GET["dateMonth"]))
-    {
-    $dateMonth = $_GET["dateMonth"]; 
-    }
 
-
-	$query 	= "SELECT * from tbl_users_206 u
-    WHERE    u.dates_year = $dateYear
-    AND       u.dates_month =$dateMonth";
+	$query 	= "UPDATE tbl_trips_206 t
+    SET      t.start_date = COALESCE(NULLIF($begin, 0), t.start_date),
+             t.end_date =  COALESCE(NULLIF($end, 0), t.end_date),
+             t.ages =  COALESCE(NULLIF($ages, 0), t.ages),
+             t.nature =  COALESCE(NULLIF($character, 0), t.nature)
+    WHERE t.t_id=" . $prodId;
     echo $query;
+	// echo $query;
 	$result = mysqli_query($connection, $query);
 
 	if($result==true) {
-        echo $result;
+        echo '1';
 	}
 
 	else die("DB query failed.");
